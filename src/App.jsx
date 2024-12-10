95bbc7a64542953bf1ed22f0f78c114c616a4804
Daniel Urbina <edu211004@gmail.com>
2024-12-10 15:43:08 -0600

    using queries

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,29 @@
+import { useQuery } from "@tanstack/react-query"
+import axios from "axios"
+
 function App() {
+  const fetchProducts = async()=>{
+    const response = await axios.get('http://localhost:3000/products')
+    return response.data
+  }
+  const {isPending, isError, data, error} = useQuery({
+    queryKey:['products'],
+    queryFn:fetchProducts
+  })
+
+  if(isPending) return <p>Fethcing data...</p>
+  if(isError) return <p>{error.message}</p>
+
   return (
-    <div>App</div>
+    <div>
+      <h2>App</h2>
+      {data.map(d=>(
+        <div key={d.id}>
+          <h3>{d.name}</h3>
+          <p>{d.description}</p>
+        </div>
+      ))}
+    </div>
   )
 }
 
